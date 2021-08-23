4992fd1549ebb4bb675e778cdde3d2abf1e2d54e
Felipe Silva Ganho <felipeganho@gmail.com>
2021-08-23 13:34:26 -0300

    controller, model e migration prontas

---
+++ database/migrations/2021_08_23_162447_create_pessoas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePessoasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pessoas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->int('cpf')->unique();
+            $table->date('dataNascimento');
+            $table->string('email');
+            $table->int('telefone');
+            $table->string('logradouro');
+            $table->string('cidade');
+            $table->string('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pessoas');
+    }
+}
